b47b64027693f9aa14751409434253c0e43067ef
Ola Reppe <112422339+olreppe@users.noreply.github.com>
2026-06-21 17:19:27 +0200

    Rename XJTAG assembly metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,22 +1,22 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JTAGConverters")]
+[assembly: AssemblyTitle("XJTAGConverters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JTAGConverters")]
+[assembly: AssemblyProduct("XJTAGConverters")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
